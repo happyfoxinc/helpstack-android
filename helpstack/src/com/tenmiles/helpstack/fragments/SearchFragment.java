c89dc0805645f168bab97d6cb7a92c25d6d2278c
Santhana <santhana@tenmiles.com>
2014-06-20 15:40:02 +0530

    Search completed

--- helpstack/src/com/tenmiles/helpstack/fragments/SearchFragment.java
+++ helpstack/src/com/tenmiles/helpstack/fragments/SearchFragment.java
@@ -1,6 +1,13 @@
 package com.tenmiles.helpstack.fragments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.MenuItemCompat.OnActionExpandListener;
@@ -11,11 +18,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.tenmiles.helpstack.R;
+import com.tenmiles.helpstack.activities.ArticleActivity;
+import com.tenmiles.helpstack.activities.SectionActivity;
 import com.tenmiles.helpstack.model.HSKBItem;
+import com.android.internal.util.*;
 
 /**
  * Search Fragment
@@ -25,8 +40,8 @@ public class SearchFragment extends HSFragmentParent {
 
 	private View rootView;
 	private SearchAdapter searchAdapter;
-	private HSKBItem[] searchableKBArticle;
-
+	private HSKBItem[] allKbArticles;
+	private ListView listView;
 
 	public SearchFragment() {
 		// Required empty public constructor
@@ -38,7 +53,32 @@ public class SearchFragment extends HSFragmentParent {
 		// Inflate the layout for this fragment
 		rootView =  inflater.inflate(R.layout.fragment_search, container, false);
 		setVisibility(false);
-		searchAdapter = new SearchAdapter(null);
+		listView = (ListView)rootView.findViewById(R.id.searchList);
+		searchAdapter = new SearchAdapter(this.allKbArticles);
+		listView.setAdapter(searchAdapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				HSKBItem kbItemClicked = (HSKBItem) searchAdapter.getItem(position);
+				if(kbItemClicked.getArticleType() == HSKBItem.TYPE_ARTICLE) {
+					//Type article
+					Intent intent = new Intent(getActivity(), ArticleActivity.class);
+					intent.putExtra("item", kbItemClicked);
+					startActivity(intent);
+					
+				} else {
+					//Type section
+					Intent intent = new Intent(getActivity(), SectionActivity.class);
+					intent.putExtra("section_item", kbItemClicked);
+					startActivity(intent);
+				}
+				
+			}
+		});
+		
 		return rootView;
 	}
 
@@ -48,8 +88,7 @@ public class SearchFragment extends HSFragmentParent {
 	}
 
 	public void doSearchForQuery(String q) {
-		// TODO Auto-generated method stub
-		Log.v("search","do querry");
+		searchAdapter.getFilter().filter(q);
 	}
 
 	public void setVisibility(boolean visible) {
@@ -61,56 +100,8 @@ public class SearchFragment extends HSFragmentParent {
 		}
 	}
 	
-	private class SearchAdapter extends BaseAdapter {
-
-		private HSKBItem[] searchResults;
-		
-		public SearchAdapter(HSKBItem[] list) {
-			this.searchResults = list;
-		}
-		
-		@Override
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return searchResults.length;
-		}
-
-		@Override
-		public Object getItem(int position) {
-			// TODO Auto-generated method stub
-			return searchResults[position];
-		}
-
-		@Override
-		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return position;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ViewHolder holder = null;
-			if(convertView == null){
-				LayoutInflater inflater = getActivity().getLayoutInflater();
-				convertView = inflater.inflate(R.layout.sectionlist_article, null);
-				holder = new ViewHolder();
-				holder.textview = (TextView)convertView.findViewById(R.id.sectionlisttextview);
-				convertView.setTag(holder);
-			}else {
-				holder = (ViewHolder)convertView.getTag();
-			}
-			holder.textview.setText(((HSKBItem)this.searchResults[position]).getSubject());
-			return convertView;
-		}
-		
-		private class ViewHolder {
-			private TextView textview;
-		}
-		
-	}
-
 	public void setKBArticleList(HSKBItem[] fetchedKbArticles) {
-		this.searchableKBArticle = fetchedKbArticles;
+		this.allKbArticles = fetchedKbArticles;
 	}
 	
 	public void addSearchViewInMenuItem(Context context, MenuItem searchItem) {
@@ -161,6 +152,102 @@ public class SearchFragment extends HSFragmentParent {
 			}
 		});
 	}
-
 	
+	private class SearchAdapter extends BaseAdapter implements Filterable{
+
+		private HSKBItem[] allKBItems;
+		private HSKBItem[] searchResults;
+		private CustomFilter filter;
+		
+		public SearchAdapter(HSKBItem[] list) {
+			this.allKBItems = list;
+		}
+		
+		@Override
+		public int getCount() {
+			if(searchResults == null) {
+				return 0;
+			}
+			return searchResults.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return searchResults[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			ViewHolder holder = null;
+			if(convertView == null){
+				LayoutInflater inflater = getActivity().getLayoutInflater();
+				convertView = inflater.inflate(R.layout.sectionlist_article, null);
+				holder = new ViewHolder();
+				holder.textview = (TextView)convertView.findViewById(R.id.sectionlisttextview);
+				convertView.setTag(holder);
+			}else {
+				holder = (ViewHolder)convertView.getTag();
+			}
+			holder.textview.setText(((HSKBItem)this.searchResults[position]).getSubject());
+			return convertView;
+		}
+		
+		private class ViewHolder {
+			private TextView textview;
+		}
+
+		@Override
+		public Filter getFilter() {
+			if(filter == null) {
+				filter = new CustomFilter();
+			}
+			return filter;
+		}
+		
+		private class CustomFilter extends Filter {
+
+			@Override
+			protected FilterResults performFiltering(CharSequence constraint) {
+				FilterResults results = new FilterResults();
+				if(constraint == null || constraint.length() == 0){
+					
+					results.values = allKBItems;
+					results.count = allKBItems.length;
+				}else {
+					// We perform filtering operation
+			        List<HSKBItem> filterList = new ArrayList<HSKBItem>();
+			         
+			        for (HSKBItem p : allKBItems) {
+			            if (p.getSubject().toUpperCase().startsWith(constraint.toString().toUpperCase()))
+			            	filterList.add(p);
+			        }
+			        HSKBItem[] values = filterList.toArray(new HSKBItem[filterList.size()]);
+			        results.values = values;
+			        results.count = filterList.size();
+				}
+				return results;
+			}
+
+			@Override
+			protected void publishResults(CharSequence constraint,
+					FilterResults results) {
+				if(results == null) {
+					notifyDataSetInvalidated();
+				}else {
+					searchResults = (HSKBItem[]) results.values;
+					notifyDataSetChanged();
+				}
+				
+			}
+			
+		}
+		
+	}
 }
