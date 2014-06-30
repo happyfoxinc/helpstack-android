620c061cf0bf3f27ffe3c07da0cfb0c3a5f3cb9c
Santhana <santhana@tenmiles.com>
2014-06-30 11:57:29 +0530

    Renamed values files with hs prefix

--- helpstack/src/com/tenmiles/helpstack/fragments/SearchFragment.java
+++ helpstack/src/com/tenmiles/helpstack/fragments/SearchFragment.java
@@ -112,7 +112,7 @@ public class SearchFragment extends HSFragmentParent {
 		
 		SearchView searchView = new SearchView(context);
 		MenuItemCompat.setActionView(searchItem, searchView);
-		searchView.setQueryHint(getString(R.string.search_hint));
+		searchView.setQueryHint(getString(R.string.hs_search_hint));
 		searchView.setSubmitButtonEnabled(true);
 
 		searchView.setOnSearchClickListener(new OnClickListener() {
