8677a98cbde3ef9962028574051f65bc9ab29714
Anirudh S <anirudh@tenmiles.com>
2014-08-01 15:31:55 +0530

    Fixed typo in included license text
    “obtaining a cop” should have been “obtaining a copy”

--- helpstack/src/com/tenmiles/helpstack/logic/HSGear.java
+++ helpstack/src/com/tenmiles/helpstack/logic/HSGear.java
@@ -2,7 +2,7 @@
 //
 //Copyright (c) 2014 HelpStack (http://helpstack.io)
 //
-//Permission is hereby granted, free of charge, to any person obtaining a cop
+//Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
 //in the Software without restriction, including without limitation the rights
 //to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -164,4 +164,3 @@ public abstract class HSGear {
 	private boolean supportHtmlMessage = false;
 
 }
-
