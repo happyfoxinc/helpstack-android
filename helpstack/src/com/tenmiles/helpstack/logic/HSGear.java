a7d6b3778949a62ffe09300bf8aa6fef6f0fa57c
Nalin Chhajer <nalinchhajer@yahoo.com>
2014-09-19 12:25:07 +0530

    Commenting and documenting HSHelpStack and HSGEar

--- helpstack/src/com/tenmiles/helpstack/logic/HSGear.java
+++ helpstack/src/com/tenmiles/helpstack/logic/HSGear.java
@@ -40,31 +40,29 @@ public abstract class HSGear {
 	public HSGear() {
 	}
 	
-	// TODO: Move all this to abstract method later
-	
 	/**
-	 * 
-	 * If u create a request add to queue and start it
-	 * 
-	 * @param section, it can be null for initial load of articles.
-	 * @param success, return HFKBItem object
-	 * @param error
-	 */
+     *
+     * @param cancelTag
+     * @param section
+     * @param queue
+     * @param success
+     * @param errorListener
+     */
 	public void fetchKBArticle(String cancelTag, HSKBItem section, RequestQueue queue,  OnFetchedArraySuccessListener success, ErrorListener errorListener ) 
 	{
 		errorListener.onErrorResponse(new VolleyError("Not implemented method"));
 	}
-	
-	/**
-	 * 
-	 * 
-	 * 
-	 * @param firstName
-	 * @param lastname
-	 * @param emailAddress
-	 * @param success, return HFUser object
-	 * @param error
-	 */
+
+    /**
+     *
+     * @param cancelTag
+     * @param firstName
+     * @param lastname
+     * @param emailAddress
+     * @param queue
+     * @param success
+     * @param errorListener
+     */
 	public void registerNewUser(String cancelTag, String firstName, String lastname, String emailAddress, RequestQueue queue, OnFetchedSuccessListener success, ErrorListener errorListener)
 	{
 		success.onSuccess(HSUser.createNewUserWithDetails(firstName, lastname, emailAddress));
@@ -83,14 +81,16 @@ public abstract class HSGear {
 	public void createNewTicket(String cancelTag, HSUser user, String subject, String body, HSUploadAttachment[] attachments, RequestQueue queue, OnNewTicketFetchedSuccessListener successListener, ErrorListener errorListener ) {
 		errorListener.onErrorResponse(new VolleyError("Not implemented method"));
 	}
-	
-	/**
-	 * 
-	 * 
-	 * @param userDetails
-	 * @param success, return HFTicket object
-	 * @param error
-	 */
+
+    /**
+     *
+     * @param cancelTag
+     * @param ticket
+     * @param user
+     * @param queue
+     * @param success
+     * @param errorListener
+     */
 	public void fetchAllUpdateOnTicket(String cancelTag, HSTicket ticket, HSUser user, RequestQueue queue, OnFetchedArraySuccessListener success, ErrorListener errorListener)
 	{
 		errorListener.onErrorResponse(new VolleyError("Not implemented method"));
@@ -108,49 +108,122 @@ public abstract class HSGear {
 	public void addReplyOnATicket(String cancelTag, String message, HSUploadAttachment[] attachments, HSTicket ticket, HSUser user, RequestQueue queue, OnFetchedSuccessListener success, ErrorListener errorListener) {
 		errorListener.onErrorResponse(new VolleyError("Not implemented method"));
 	}
-	
+
+    /**
+     * Set this parameter, if gear is not implementing handling of Issues. Doing this, default email client will be open with given support Email Address.
+     * Then there is no need to implement issues fetching related methods.
+     *
+     * @default  it is considered that gear is gonna implement ticket fetching.
+     *
+     * @param companySupportEmailAddress
+     */
 	public void setNotImplementingTicketsFetching(String companySupportEmailAddress) {
 		implementsTicketFetching = false;
 		this.companySupportEmailAddress = companySupportEmailAddress;
 	}
-	
+
+    /**
+     *
+     * @return Company support email address set in {@link #setNotImplementingTicketsFetching(java.lang.String) setNotImplementingTicketsFetching}
+     */
+    public String getCompanySupportEmailAddress() {
+        return companySupportEmailAddress;
+    }
+    /**
+     * Returns if gear have implemented Ticket Fetching. Modify this parameter using {@link #setNotImplementingTicketsFetching(java.lang.String) setNotImplementingTicketsFetching}
+     *
+     * @default true
+     *
+     * @return
+     */
+    public boolean haveImplementedTicketFetching() {
+        return implementsTicketFetching;
+    }
+    /**
+     * Set this parameter, if gear is not implementing handling of FAQ. Doing this, FAQ will be fetched from article path.
+     * Then there is no need to implement issues fetching related methods.
+     *
+     * @default  it is considered that gear is gonna implement email fetching.
+     *
+     * @param articleResid
+     */
 	public void setNotImplementingKBFetching (int articleResid) {
 		implementsKBFetching = false;
 		this.articleResid = articleResid;
 	}
-	
+
+    /**
+     *
+     * @return Local article id set in {@link #setNotImplementingKBFetching(int) setNotImplementingKBFetching}
+     */
+    public int getLocalArticleResourceId() {
+        return articleResid;
+    }
+
+    /**
+     * Returns if gear have implemented KB Fetching. Modify this parameter using {@link #setNotImplementingKBFetching(int) setNotImplementingKBFetching}
+     *
+     * @default true
+     *
+     * @return
+     */
+    public boolean haveImplementedKBFetching() {
+        return implementsKBFetching;
+    }
+
+    /**
+     *
+     * If this parameter is set, message written by user in chat screen and new issue screen will be returned in HTML format.
+     *
+     * @default false
+     *
+     * @param htmlEnabled
+     */
 	public void uploadMessageAsHtmlString(boolean htmlEnabled) {
 		this.supportHtmlMessage = htmlEnabled;
 	}
-	
+
+    /**
+     * returns if gear can upload message as html string.
+     *
+     * @default false
+     *
+     * @return
+     */
 	public boolean canUplaodMessageAsHtmlString() {
 		return supportHtmlMessage;
 	}
-	
-	public boolean haveImplementedTicketFetching() {
-		return implementsTicketFetching;
-	}
-	
-	public boolean haveImplementedKBFetching() {
-		return implementsKBFetching;
-	}
-	
-	public int getLocalArticleResourceId() {
-		return articleResid;
-	}
-	
-	public String getCompanySupportEmailAddress() {
-		return companySupportEmailAddress;
-	}
-	
+
+
+    /**
+     *
+     * Sets maximum number of attachment gears can handle.
+     *
+     * @default 1
+     *
+     * @param numberOfAttachmentGearCanHandle
+     */
 	public void setNumberOfAttachmentGearCanHandle (int numberOfAttachmentGearCanHandle) {
 		this.numberOfAttachmentGearCanHandle = numberOfAttachmentGearCanHandle;
 	}
-	
+
+    /**
+     *
+     * @return maximum number of attachment gear can handle.
+     * @default is 1
+     */
 	public int getNumberOfAttachmentGearCanHandle() {
 		return numberOfAttachmentGearCanHandle;
 	}
-	
+
+
+
+
+
+    ////////////////////////////////////////////////////
+    /////////////   Private Variables   ///////////////
+    ///////////////////////////////////////////////////
+
 	private int numberOfAttachmentGearCanHandle = 1;
 	
 	// If this is true, we don't call kb article functions, will open email app is required.
